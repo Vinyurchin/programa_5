9dd3bb862bb85cba852f290b01c8ce993b31316e
Vinyurchin <diegoc.g56@gmail.com>
2025-03-05 12:43:39 -0600

    Primero archivos

---
+++ middlewares/logger.js
@@ -0,0 +1,5 @@
+const logger = (req, res, next) => {
+    const fecha = new Date().toDateString.ISOString();
+    const metodo = req.method;
+    cons
+}
---
+++ service.js
@@ -0,0 +1,12 @@
+const services = [
+    {
+        name: "auth",
+        url: "http://localhost:3000"
+        path: "api/auth"
+    },
+    {
+        name: "users",
+        url: "http://localhost:3001"
+        path: "api/users"
+    }
+]
